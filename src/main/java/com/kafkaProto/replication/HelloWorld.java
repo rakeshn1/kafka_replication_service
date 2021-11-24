664d7826f78ff4ad6ba3402de7a219cdf3f8c897
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-11-24 14:03:27 -0800

    file check

--- src/main/java/com/kafkaProto/replication/HelloWorld.java
+++ src/main/java/com/kafkaProto/replication/HelloWorld.java
@@ -1,7 +1,22 @@
 package com.kafkaProto.replication;
 
+import java.io.File;
+import java.io.IOException;
+
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello Replicas");
+
+        // check if file exist
+        File temp = new File("com/kafkaProto/topics");
+        boolean exists = temp.exists();
+
+        // create new file
+        File myObj = new File("filename.txt");
+        if (myObj.createNewFile()) {
+            System.out.println("File created: " + myObj.getName());
+        } else {
+            System.out.println("File already exists.");
+        }
     }
 }
