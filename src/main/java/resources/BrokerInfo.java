dce55c7dada04b9e594c87a1663815745dd8d7f4
Anil Gubbala <anil.gubbala@sjsu.edu>
2021-12-13 15:06:53 -0800

    create and append logs code added

--- src/main/java/resources/BrokerInfo.java
+++ src/main/java/resources/BrokerInfo.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 public final class BrokerInfo {
     public static List<String> hosts = new ArrayList<>(Arrays.asList(
-            "192.168.106.100"
+            "192.168.43.53"
 //            "192.168.0.1",
 //            "192.168.0.2",
 //            "192.168.0.3"
     ));
 
-    public static String myIp = "192.168.43.53";
+    public static String myIp = "localhost";
 
     public static List<String> getHosts() {
         return hosts;
